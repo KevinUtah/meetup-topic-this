63f199178eabee391793d008ec2f2798f19f3d50
Kevin Black <Kevin_Jay_Black@hotmail.com>
2017-01-31 09:57:55 -0700

    feat(core) Populate html and js files

---
+++ script.js
@@ -0,0 +1,17 @@
+// console.log("hello, world!");
+// console.log(document.title);
+// alert(document.title);
+
+/*
+parts of a house:
+window
+door
+floor
+
+*/
+
+// "Use strict";
+
+/*
+displaying the parts of the house
+*/
